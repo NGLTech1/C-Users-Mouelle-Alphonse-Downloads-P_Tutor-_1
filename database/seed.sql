cfb505aadb4cb3a0ca5fc0c9393566ff57a11927
Edson Irvann <dhvnmn448k@privaterelay.appleid.com>
2026-06-22 17:23:38 +0200

    Ajout des données de test

---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO users(nom,email,password)
+VALUES
+('admin','admin@gmail.com','123456'),
+('user1','user1@gmail.com','123456');
+INSERT INTO counters(user_id,value)
+VALUES
+(1,0),
+(2,0);
